0eac56049e4fe7d9ead858c2508c2810f4d283ca
naveenna3 <naveena2000@gmail.com>
2023-03-17 16:47:56 +0530

    Initial Commit

---
+++ SimpleApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SimpleApp
+//
+//  Created by Intel_101 on 17/03/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SimpleApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SimpleApp
+//
+//  Created by Intel_101 on 17/03/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
